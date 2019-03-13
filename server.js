0b59465f36778d8ea092fec9316eb8778e7c71be
Alejandro Lavado <alejandro.lvd@gmail.com>
2019-03-12 22:40:11 -0300

    configura heroku

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 const express = require('express')
 const app = express()
+const hbs = require('hbs')
+require('./hbs/helpers')
+
+const port = process.env.PORT
 
 app.use(express.static(__dirname + '/public'))
 
-// app.get('/', (req, res) => {
-//     let salida = {
-//         nombre: 'fernando',
-//         edad: 32,
-//         url: req.url
-//     }
-//     res.send(salida)
-// })
+// Express HBS engine
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs')
 
-app.listen(3000, () => {
-    console.log('Escuchando peticiones en el puerto 3000')
+app.get('/', (req, res) => {
+    res.render('home', {
+        nombre: 'alejandro lavado'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${port}`)
 })
